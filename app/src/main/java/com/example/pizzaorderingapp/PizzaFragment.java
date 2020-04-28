5a7feb1920e263a274bf39f26dcb3be446e6b2b0
JYK_SB2 <ch0005649@gmail.com>
2020-04-29 05:34:29 +1200

    fourth commit image load success apply to sides and drinks

--- app/src/main/java/com/example/pizzaorderingapp/PizzaFragment.java
+++ app/src/main/java/com/example/pizzaorderingapp/PizzaFragment.java
@@ -1,5 +1,7 @@
 package com.example.pizzaorderingapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -11,16 +13,21 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
@@ -35,6 +42,8 @@ public class PizzaFragment extends Fragment {
 
     LinearLayout mainLayout;
     RadioGroup radioGroup;
+    String[] pizzaNames= {"pizzaComb.jpg", "pizzaVege.png", "pizzaMeat.png"};
+    Bitmap [] pizzaImg= new Bitmap[pizzaNames.length];
     public PizzaFragment() {
         // Required empty public constructor
     }
@@ -47,7 +56,7 @@ public class PizzaFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_pizza, container, false);
         // Inflate the layout for this fragment
 
-
+        //create new RadioGroup in for the menu
         radioGroup = new RadioGroup(getActivity());
         radioGroup.setOrientation(LinearLayout.VERTICAL);
 
@@ -60,6 +69,9 @@ public class PizzaFragment extends Fragment {
 
     }
 
+/*
+/class for fetching data from server
+ */
     class LoadProducts extends AsyncTask<String, String, String>{
 
         @Override
@@ -71,22 +83,6 @@ public class PizzaFragment extends Fragment {
                 URL url = new URL(urlAddress[0]);
                 URLConnection connection = url.openConnection();
 
-                //*************parameters necessary???????**************
-                /*
-                String parameters = URLEncoder.encode("productType", "UTF-8");
-                parameters += "=" + URLEncoder.encode(urlAddress[1], "UTF-8");
-
-                //output stream
-                connection.setDoOutput(true);
-                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
-
-                //send param
-                outputStreamWriter.write(parameters);
-                outputStreamWriter.flush();
-
-
-                 */
-                //
                 BufferedReader bufferReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 stringBuilder = new StringBuilder();
                 String line = null;
@@ -113,42 +109,67 @@ public class PizzaFragment extends Fragment {
                 //the main loop to read colume one by one
                 for (int i = 0; i < jsonArray.length(); i++) {
                     JSONObject jsonObject = jsonArray.getJSONObject(i);
+
                     //***************get product information****************
                     String productName = jsonObject.getString("name");
                     String productPrice = "$"+ jsonObject.getString("price");
 
                     //go set the UI in another method
-                    setLayout(productName, productPrice);
-                    mainLayout.removeAllViews();
-                    mainLayout.addView(radioGroup);
+                    setLayout(productName, productPrice, pizzaNames[i]);
                 }
+
+                //after adding all products in the radiogroup, add it to the linearlayout
+                mainLayout.removeAllViews();
+                mainLayout.addView(radioGroup);
             }
             catch(Exception e){
                 e.printStackTrace();
             }
         }
-
-        //set the product information on the xml file
-        private void setLayout(String name, String price){
+    }
 
 
-            //make new textviews
-            RadioButton rbProduct = new RadioButton(getActivity());
+    //set the product information on the xml file
+    private void setLayout(String name, String price, String imgName){
 
-            //setting layout params below
 
-            //product name
-            LinearLayout.LayoutParams lp =new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT);
-            lp.gravity = Gravity.CENTER;
-            rbProduct.setLayoutParams(lp);
-            rbProduct.setText(name + "       Price:"+ price);
-            rbProduct.setTextSize(20);
+        //make new textviews
+        RadioButton rbProduct = new RadioButton(getActivity());
+        TextView txtPrice = new TextView(getActivity());
 
-            //add to the group
-            radioGroup.addView(rbProduct);
+        //setting layout params below
 
-            //add images
-        }
+        //product name
+        LinearLayout.LayoutParams lp =new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.gravity = Gravity.CENTER;
+        rbProduct.setLayoutParams(lp);
+        rbProduct.setText(name);
+        rbProduct.setTextSize(20);
+
+        //product price
+        lp.gravity = Gravity.CENTER;
+        txtPrice.setLayoutParams(lp);
+        txtPrice.setText("  Price: " +price);
+        txtPrice.setTextSize(20);
+
+        //add images
+        /*
+        /Picasso is a powerful library which doesn't need asynctask at all but have
+        all the function of thread loading and cache deleting
+         */
+        ImageView pizzaImg = new ImageView(getActivity());
+        Picasso.get()
+                .load(UrlSettings.pizzaImgs+imgName)
+                .error(R.drawable.progress)
+                .placeholder(R.drawable.progress_animation)
+                .resize(800,800)
+                .into(pizzaImg);
+
+        //add to the group
+        radioGroup.addView(rbProduct);
+        radioGroup.addView(txtPrice);
+        radioGroup.addView(pizzaImg);
+
     }
 }
--- app/src/main/java/com/example/pizzaorderingapp/UrlSettings.java
+++ app/src/main/java/com/example/pizzaorderingapp/UrlSettings.java
@@ -2,4 +2,20 @@ package com.example.pizzaorderingapp;
 
 public class UrlSettings {
     public static String searchProducts = "http://10.0.2.2/PizzaOrderingApp/SearchProducts.php";
+    public static String pizzaImgs = "http://10.0.2.2/PizzaOrderingApp/images/";
+    public static String pizzaComb = "http://10.0.2.2/PizzaOrderingApp/images/pizzaComb.jpg";
+    public static String pizzaVege = "http://10.0.2.2/PizzaOrderingApp/images/pizzaVege.png";
+    public static String pizzaMeat = "http://10.0.2.2/PizzaOrderingApp/images/pizzaMeat.png";
+    /*
+    pizza images from Supreme Pizza from the US
+    http://www.supremepizza.us/
+     */
+
+    public static String chips = "";
+    public static String rings = "";
+    public static String hash = "";
+    /*
+
+     */
+
 }
